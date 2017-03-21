9b1bd4fbd4b5b350a20c6536a3aecf00852c0d47
Veronika Machackova <veronikam@Veronikas-MacBook-Pro.local>
2017-03-21 11:43:26 +0100

    java

---
+++ js/java.js
@@ -0,0 +1,10 @@
+let name = "Veronika";
+
+let numKids = 0;
+let partnerName = "Rasmus";
+
+let GeoLoc= "Copenhagen";
+
+let jobTitle= "Super Woman";
+
+console.log(" You will be a " + jobTitle + " in " + GeoLoc + " and not married to " + partnerName + " with " + numKids + " kids.");
---
+++ js/main.js
@@ -0,0 +1,6 @@
+let anim = document.querySelector("#anim");
+anim.addEventListener('click', startAnimation);
+
+function startAnimation(){
+    anim.classList.add('enable');
+}
